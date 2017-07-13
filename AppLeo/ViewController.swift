1b5e13019e651b9a1b7f6a7566a8c5387ef4587c
IskanderValiev <iskand.valiev@yandex.ru>
2017-07-13 11:08:58 +0300

    changed share button size, added icon

--- AppLeo/ViewController.swift
+++ AppLeo/ViewController.swift
@@ -13,6 +13,31 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mirrorLabelText: UILabel!
     
+    @IBOutlet weak var myShareButtonProperty: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        textTV.layer.borderWidth = 1.0
+        textTV.layer.borderColor = UIColor.black.cgColor
+        
+        myShareButtonProperty.sizeToFit()
+    }
+
+    
+    
+    @IBAction func translate(_ sender: UIButton) {
+        mirrorLabelText.text = textTV.text
+        
+        UIView.animate(withDuration: 1.0) { 
+          self.mirrorLabelText.transform = CGAffineTransform(scaleX: -1, y: 1)
+        }
+        mirrorLabelText.sizeToFit()
+        
+    }
     
     @IBAction func myShareButton(_ sender: UIButton) {
         // Hide the keyboard
@@ -25,25 +50,6 @@ class ViewController: UIViewController {
             // We have contents so display the share sheet
             displayShareSheet(shareContent: mirrorLabelText.text!)
         }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        textTV.layer.borderWidth = 1.0
-        textTV.layer.borderColor = UIColor.black.cgColor
-    }
-
-    
-    
-    @IBAction func translate(_ sender: UIButton) {
-        mirrorLabelText.text = textTV.text
-        
-        UIView.animate(withDuration: 1.0) { 
-          self.mirrorLabelText.transform = CGAffineTransform(scaleX: -1, y: 1)
-        }
-        mirrorLabelText.sizeToFit()
         
     }
     
