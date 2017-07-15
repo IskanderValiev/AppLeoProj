245d916fd963381f8e27bb40b3a102a204c29430
IskanderValiev <iskand.valiev@yandex.ru>
2017-07-15 08:52:36 +0300

    new font

---
+++ AppLeo/DecodingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DecodingViewController.swift
+//  AppLeo
+//
+//  Created by Iskander Valiev on 7/14/17.
+//  Copyright © 2017 ITIS Team #3. All rights reserved.
+//
+
+import UIKit
+
+class DecodingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
